dd3c3d79302bbf0441422d737022e7b96c7073ba
IkaWaAyuMu <pungpond4450@gmail.com>
2021-01-19 16:09:57 +0700

    lab12_2 Created.

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,22 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M], int size, int x, int y)
+{
+    for (int i = 0 ; i < 30 ; i++) for (int j = 0 ; j < 70 ; j++) image[i][j] = image[i][j] || sqrt(pow(i-x, 2)+pow(j-y, 2)) <= size-1;
+}
+
+void showImage(const bool image[][M])
+{
+    for (int i = 0 ; i < 72 ; i ++) cout << "-";
+    cout << endl;
+    for (int i = 0 ; i < 30 ; i ++)
+    {
+        cout << "|";
+        for (int j = 0 ; j < 70 ; j++) cout << (image[i][j] ? "*" : " ");
+        cout << "|\n";
+    }
+    for (int i = 0 ; i < 72 ; i ++) cout << "-";
+    cout << endl;
+}
